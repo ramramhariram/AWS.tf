e37eef090b3b960c949380f3fb2c891e72277cb5
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2020-12-16 16:40:24 -0800

    updating region as a variable

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 provider "aws" {
   version = "~> 3.0"
-  region  = "us-east-1"
-  #shared_credentials_file = "/Users/hariram/.aws/"
-  #profile = "integrationlab"
-  #access_key = "AWS_ACCESS_KEY_ID"
-  #secret_key = "AWS_SECRET_ACCESS_KEY"
+  region  = var.region 
 }
 
